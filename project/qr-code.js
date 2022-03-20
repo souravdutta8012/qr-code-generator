7d6c831b756d9106f2323de0fb1774097341c4a0
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2022-03-20 18:46:26 +0530

    changes

--- project/qr-code.js
+++ project/qr-code.js
@@ -2,18 +2,24 @@ const QRCode = require('qrcode');
 const { jsPDF } = require('jspdf');
 const setting = require('../settings.json');
 
-text = `Voltage: ${setting.Voltage}V. Ampere: ${setting.Ampere}A. Manufacturing Date: ${setting.Date}.`;
+text = `Voltage: ${setting.Voltage}V. Ampere: ${setting.Ampere}A. Manufacturing Date: ${setting.Date}. Company Name: ${setting.CompanyName}.`;
 
 QRCode.toDataURL(text, function (err, imgData) {
     if (err) {
         console.log(err);
     }
-    const doc = new jsPDF('p', 'mm', [108, 140]);
-    let y = 1.5;
-    for (let i = 0; i < 5; i++) {
-        doc.addImage(imgData, 'PNG', 17, y + (25 - 20) / 2, 20, 20);
-        doc.addImage(imgData, 'PNG', 71, y + (25 - 20) / 2, 20, 20);
-        y += 28;
+
+    const doc = new jsPDF('p', 'mm', [101.6, 101.6]); // W X H
+    // const companyTextWidth = doc.getTextWidth(setting.CompanyName);
+    // doc.text(13,  y + 0, setting.CompanyName);
+    // doc.setFontSize(9);
+
+    let y = 0;
+    for (let i = 0; i < 4; i++) {
+        doc.addImage(imgData, 'PNG', 15.2, y + 0, 20, 20);
+        doc.addImage(imgData, 'PNG', 65.7, y + 0, 20, 20);
+        y += 26.5;
     }
+
     doc.save(`../output/${setting.FileName}.pdf`);
 });
