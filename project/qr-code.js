20c9b9de4b0063d72a05779bbb2dbdb2a1fe7429
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2022-03-26 22:51:22 +0530

    changes

--- project/qr-code.js
+++ project/qr-code.js
@@ -10,28 +10,26 @@ QRCode.toDataURL(text, function (err, imgData) {
     }
 
     const docPrinter = new jsPDF('p', 'mm', [101.6, 101.6]); // W X H
-
     let y = 0;
     for (let i = 0; i < 4; i++) {
         docPrinter.addImage(imgData, 'PNG', 15.2, y + 0, 20, 20);
         docPrinter.addImage(imgData, 'PNG', 65.7, y + 0, 20, 20);
         y += 26.5;
     }
-
     docPrinter.save(`../output/${setting.FileName}.pdf`);
 
     const docA4 = new jsPDF('p', 'mm', "a4"); // W X H
     // const companyTextWidth = docA4.getTextWidth(setting.CompanyName);
     // docA4.text(13,  y + 0, setting.CompanyName);
     // docA4.setFontSize(9);
-
     y = 0;
     let width = docA4.internal.pageSize.getWidth();
-    let height = docA4.internal.pageSize.getHeight();
-    docA4.addImage(imgData, 'PNG', width / 8 - 25 / 2, y + 0, 25, 25);
-    docA4.addImage(imgData, 'PNG', width / 4 - 25 / 2, y + 0, 25, 25);
-    docA4.addImage(imgData, 'PNG', width / 2 - 25 / 2, y + 0, 25, 25);
-    docA4.addImage(imgData, 'PNG', width / 1 - 25 / 2, y + 0, 25, 25);
-
+    for (let i = 0; i < 8; i++) {
+        docA4.addImage(imgData, 'PNG', width / 8 - 25 / 2, y + 0, 25, 25);
+        docA4.addImage(imgData, 'PNG', width * 3 / 8 - 25 / 2, y + 0, 25, 25);
+        docA4.addImage(imgData, 'PNG', width * 5 / 8 - 25 / 2, y + 0, 25, 25);
+        docA4.addImage(imgData, 'PNG', width * 7 / 8 - 25 / 2, y + 0, 25, 25);
+        y += 37;
+    }
     docA4.save(`../output/${setting.FileName}-a4.pdf`);
 });
--- run.bat
+++ run.bat
@@ -4,5 +4,6 @@ mkdir output
 cd project
 call npm install
 call npm run build
+cd ..
 echo Done! Get you QR pdf from output folder....
 @pause
